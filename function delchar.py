335a2e2aa1e754ef0005943bb5e63f9717dfdb5a
Sai230803 <112486803+Sai230803@users.noreply.github.com>
2022-09-15 09:20:21 +0530

    Update function delchar.py

--- function delchar.py
+++ function delchar.py
@@ -1,9 +1,11 @@
+def charrem (s,r):
+    sp=""
+    for i in (s):
+         if(i==r):
+             continue
+         else :
+             sp=sp+i
+    print(sp)         
 s=str(input("sentence: "))
 r=input("The character to be removed : ")
-sp=""
-for i in (s):
-    if(i==r):
-        continue
-    else :
-        sp=sp+i
-print(sp)
+charrem(s,r)
