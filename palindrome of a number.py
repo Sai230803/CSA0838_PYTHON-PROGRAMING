79569de3d51af426d860a4d1b3272df29c98537d
Sai230803 <112486803+Sai230803@users.noreply.github.com>
2022-10-12 14:14:07 +0530

    Create palindrome of a number.py

--- palindrome of a number.py
+++ palindrome of a number.py
@@ -1,12 +1,11 @@
-num = int(input("Enter a value:"))  
-temp = num  
-rev = 0  
-while(num > 0):  
-    dig = num % 10  
-    rev = rev * 10 + dig  
-    num= num // 10  
-if(temp == rev):  
+n = int(input("Enter a value:"))  
+temp = n  
+r = 0  
+while(n > 0):  
+    d = n % 10  
+    r = r * 10 + d  
+    n= n // 10  
+if(temp == r):  
     print("This value is a palindrome number!")  
 else:  
     print("This value is not a palindrome number!")  
-
