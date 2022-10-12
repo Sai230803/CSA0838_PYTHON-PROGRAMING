d239f8f8309777e4c1f334ebe134982ba25075ff
Sai230803 <112486803+Sai230803@users.noreply.github.com>
2022-10-12 14:07:25 +0530

    Update palindrome of a number.py

--- palindrome of a number.py
+++ palindrome of a number.py
@@ -1,11 +1,12 @@
-def ispalindrome(s) :
-    rev =''.join(reversed(s))
-    if (s==rev):
-        return True
-    return False
-s=input("enter the word")
-ans= ispalindrome(s)
-if(ans):
-    print("yes")
-else:
-    print("no")
+num = int(input("Enter a value:"))  
+temp = num  
+rev = 0  
+while(num > 0):  
+    dig = num % 10  
+    rev = rev * 10 + dig  
+    num= num // 10  
+if(temp == rev):  
+    print("This value is a palindrome number!")  
+else:  
+    print("This value is not a palindrome number!")  
+
